0d69722c2ad87634ca502c6225ea204cf32f9e48
Phil Billingsby <philbillingsby@gmail.com>
2020-01-11 11:25:17 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,23 @@
-binding.pry
 class PigLatinizer
   attr_reader :input
+
   def initialize(input)
     @input = input
+    starts_with?
   end
-
-  def piglatinizer
-    binding.pry
-    ^/[aeioui]/i.match(@input)
+  
+  def starts_with?
+    new = self.input.split
+    if self.input.downcase.start_with?(/^[aeiou]/) && new.count == 1
+      "#{@input + "way"}"
+    else
+      new.map do |word|
+        if word.downcase.start_with?(/^[aeiou]/)
+          word << "way"
+        elsif !word.downcase.start_with?(/^[aeiou]/)
+          word[1..-1] + word[0] + "ay"
+        end
+      end
+    end
   end
 end
