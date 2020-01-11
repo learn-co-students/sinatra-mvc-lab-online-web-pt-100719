5c96f898da0c524aa75263f1c2e25d1199e487c3
Phil Billingsby <philbillingsby@gmail.com>
2020-01-11 12:13:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,4 +5,9 @@ class App < Sinatra::Base
   get "/" do
     erb :user_input
   end
+  post '/piglatinize' do
+    new_pig = PigLatinizer.new #New PigLatinizer object
+    @string = new_pig.piglatinize(params[:user_input]) # Create instance variable from form input
+    erb :piglatinize
+  end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,22 +1,21 @@
 class PigLatinizer
-  attr_reader :input
-
-  def initialize(input)
-    @input = input
-    starts_with?
-  end
-  
-  def starts_with?
-    new = self.input.split
-    if self.input.downcase.start_with?(/^[aeiou]/) && new.count == 1
-      "#{@input + "way"}"
-    else
-      new.map do |word|
-        if word.downcase.start_with?(/^[aeiou]/)
-          word << "way"
-        elsif !word.downcase.start_with?(/^[aeiou]/)
-          word[1..-1] + word[0] + "ay"
-        end
+  # Must figure out if input is one word or a string of multiple words
+  def piglatinize(words)
+    new = words.split
+    if words.start_with?(/^[aeiou]/) && new.count == 1
+      puts "WORD WITH VOWEL"
+      return (words + "way")
+    elsif !words.start_with?(/^[aeiou]/) && new.count == 1
+      puts "WORD WITH CONST"
+      return (words[1..-1] + words[0] + "ay")
+    end
+    new.map do |word|
+      if word.downcase.start_with?(/^[aeiou]/) && new.count > 1
+        puts "WORDS WITH VOWEL"
+        return word << "way"
+      elsif !word.downcase.start_with?(/^[aeiou]/) && new.count > 1
+        puts "WORDS WITH CONST"
+        return word[1..-1] + word[0] + "ay"
       end
     end
   end
